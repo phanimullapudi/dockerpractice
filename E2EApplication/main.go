276d82578809f865e6c3c01b0855423ed6177b81
Phani Mullapudi <phani.mullapudi@gmail.com>
2021-08-29 11:17:16 -0500

    Added connection and retrieving code

--- E2EApplication/main.go
+++ E2EApplication/main.go
@@ -2,13 +2,45 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
 
 	redisConnectRepo "github.com/phanimullapudi/dockerpractice/E2EApplication/internal/database"
 )
 
-type Poll struct {
-	Name   string `json:"name"`
-	Choice int    `json:"age"`
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to the Home Page")
+	fmt.Println("Endpoint Hit: homePage")
+
+	redisclient := redisConnectRepo.ConnectDB()
+	err := redisclient.Set("name", "Elliot", 0).Err()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+}
+
+func returnAllItems(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Endpoint Hit: returnAllItems")
+
+	redisclient := redisConnectRepo.ConnectDB()
+	val, err := redisclient.Get("name").Result()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Fprintln(w, val)
+
+}
+
+func handleRequests() {
+	myRouter := mux.NewRouter().StrictSlash(true)
+	myRouter.HandleFunc("/", homePage)
+	myRouter.HandleFunc("/all", returnAllItems)
+
+	log.Fatal(http.ListenAndServe(":1000", myRouter))
 }
 
 func main() {
@@ -20,5 +52,5 @@ func main() {
 		fmt.Println(err)
 	}
 	fmt.Println(pong)
-
+	handleRequests()
 }
